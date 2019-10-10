cb46139572162a79c8a8da0ca5aa1d60f34bf680
acarmstead <ashtona@umich.edu>
2019-10-10 16:01:29 -0400

    Initial Commit

---
+++ JobSearchPrograms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JobSearchPrograms
+//
+//  Created by Ashton Armstead on 10/10/19.
+//  Copyright © 2019 Ashton Armstead. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
